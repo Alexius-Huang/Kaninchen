0c40296456174ec4e0f824fd1b1c383f6b0e014f
Maxwell-Alexius <destinydragon85614@gmail.com>
2017-08-10 10:00:42 +0800

    [Feature] Implement Kaninchen::DataStructure::Tree#each_node in Preorder Case

--- lib/kaninchen/data_structure/tree.rb
+++ lib/kaninchen/data_structure/tree.rb
@@ -12,6 +12,35 @@ module Kaninchen
         end
         @root.send(:set_tree_node_data, tree: self)
       end
+
+      def each_node(type = :preorder)
+        case type
+        when :preorder
+          yield self.root
+          recursive_preorder_loop(self.root) { |node| yield node }
+        end
+      end
+
+      def each_node_with_index(type = :preorder)
+        case
+        when (type === :preorder or type = :depth_first)
+          yield self.root, 0
+          index = 1
+          recursive_preorder_loop(self.root) do |node|
+            yield node, index
+            index += 1
+          end
+        end
+      end
+
+      private
+
+      def recursive_preorder_loop(parent_node)
+        parent_node.children.each do |child_node|
+          yield child_node
+          recursive_preorder_loop(child_node) { |node| yield node }
+        end
+      end
     end
 
     class Node
--- spec/kaninchen/data_structure/tree_spec.rb
+++ spec/kaninchen/data_structure/tree_spec.rb
@@ -3,8 +3,21 @@
 require 'spec_helper'
 
 RSpec.describe Kaninchen::DataStructure::Tree do
-  let(:tree) { described_class.new 'ROOT' }
-  let(:node) { Kaninchen::DataStructure::Node.new 'ROOT' }
+  let(:tree)   { described_class.new 'ROOT' }
+  let(:root)   { Kaninchen::DataStructure::Node.new 'ROOT' }
+  let(:node_1) { Kaninchen::DataStructure::Node.new 'CHILD1' }
+  let(:node_2) { Kaninchen::DataStructure::Node.new 'CHILD2' }
+  let(:node_3) { Kaninchen::DataStructure::Node.new 'CHILD3' }
+  let(:node_4) { Kaninchen::DataStructure::Node.new 'CHILD4' }
+  let(:node_5) { Kaninchen::DataStructure::Node.new 'CHILD5' }
+  let(:node_6) { Kaninchen::DataStructure::Node.new 'CHILD6' }
+  let(:sample_tree_struct) do
+    {
+      node_1 => [node_2, node_3],
+      node_4 => nil,
+      node_5 => node_6
+    }
+  end
 
   describe 'initialization' do
     it 'can initialize with a root value' do
@@ -12,7 +25,7 @@ RSpec.describe Kaninchen::DataStructure::Tree do
     end
 
     it 'can initialize with a root value as node' do
-      tree = described_class.new node
+      tree = described_class.new root
       expect(tree.class).to eq described_class
     end
 
@@ -30,15 +43,42 @@ RSpec.describe Kaninchen::DataStructure::Tree do
 
   describe 'instance methods' do
     describe 'looping methods' do
-      context '#each_node' do
-        it 'should loop through tree default with preorder-traversal'
-        it 'should loop through tree specified :preorder or :depth_first as preorder-traversal'
+      context '#each_node & #each_node_with_index' do
+        let(:preorder) { [tree.root, node_1, node_2, node_3, node_4, node_5, node_6] }
+
+        before { tree.root << sample_tree_struct }
+
+        shared_examples 'preorder case' do |params|
+          it "should loop through tree specified #{params} as preorder-traversal" do
+            index = 0
+            tree.each_node(params) do |node|
+              expect(node).to eq preorder[index]
+              index += 1
+            end
+
+            tree.each_node_with_index(params) { |node, index| expect(node).to eq preorder[index] }
+          end
+        end
+        
+        it 'should loop through tree default with preorder-traversal' do
+          index = 0
+          tree.each_node do |node|
+            expect(node).to eq preorder[index]
+            index += 1
+          end
+
+          tree.each_node_with_index { |node, index| expect(node).to eq preorder[index] }
+        end
+
+        it_should_behave_like 'preorder case', :preorder
+        it_should_behave_like 'preorder case', :depth_first
+
         it 'should loop through tree specified :inorder as inorder-traversal'
         it 'should loop through tree specified :postorder as postorder-traversal'
         it 'should loop through tree specified :levelorder or :breath_first as levelorder-traversal'
       end
 
-      context 'each_leaves' do
+      context '#each_leaf' do
         it 'should loop through leaves of the tree'
       end
     end
@@ -70,11 +110,11 @@ RSpec.describe Kaninchen::DataStructure::Tree do
         it 'should return the degree of the tree (which is the degree of root)'
       end
 
-      context 'edges' do
+      context '#edges' do
         it 'should return the count of the edges of the tree'
       end
 
-      context 'height' do
+      context '#height' do
         it 'should return the height of the tree'
       end
     end
